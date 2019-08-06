bf1fb8a562fe9b0feceef0c1912d0d14173b65e4
pinakigitub <pinaki.best@gmail.com>
2019-08-06 13:58:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HubConnection,HubConnectionBuilder } from '@aspnet/signalr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private _hubConnection: HubConnection;
   title = 'UI';
+  messages: string[] = [];
+  nick = '';
+  
+  ngOnInit() {
+    this._hubConnection = new HubConnectionBuilder()
+                        .withUrl("https://pinakisignalr.herokuapp.com/Hubs/Values").build();
+
+    this._hubConnection
+      .start()
+      .then(() => 
+      console.log('Connection started!'))
+      .catch(err => console.log('Error while establishing connection :('));
+
+      this._hubConnection.on('Add', (nick: string, receivedMessage: string) => {
+        
+        this.messages.push(nick);
+      });
+
+      this._hubConnection.on('Delete', (nick: string, receivedMessage: string) => {
+      
+        console.log('Ms to be deleted '+ nick);
+        const index: number = this.messages.indexOf(nick);
+        if (index !== -1) {
+            this.messages.splice(index, 1);
+        } 
+      });
+
+    }
 }
